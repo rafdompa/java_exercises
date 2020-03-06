0c0d3356e5c56c472b55ae8e9d18868a57bdbdf1
Rafael Dominguez <rafdompa@gmail.com>
2020-03-05 23:53:51 -0500

    Fixed build configurations; added GuessingGame exercise

---
+++ src/com/exercises/a03_guessing_game/GuessingGame.java
@@ -0,0 +1,37 @@
+package com.exercises.a03_guessing_game;
+
+import java.util.Scanner;
+
+public class Main {
+
+    /* <summary>
+            INSTRUCTIONS:
+                * The user (driver) will call this method with a number; the method will determine what prize,
+                    if any, has been won
+                * if number is divisible by 5, return "You won a car!"
+                * if number is divisible by 2, return "You won a mouse!"
+                * if number is divisible by 3, return "You won a house!!"
+                * any other number, return "You lost, play again"
+
+            HINT:
+                * Good time to use the modulus operator (look it up)
+                * this is a self-testing method; it does not run by itself;
+                    it's called by a test method (it's kinda like a driver)
+
+            EXAMPLE:
+       
+                |   INPUT     | OUTPUT                     |
+                |------------------------------------------|
+                | 30          |  "You won a car!"             |
+                |------------------------------------------|
+
+    */
+    public static String generatePrizeBasedOnNumber(int number) {
+        String message = "";
+
+        //do calculations here
+
+        //output here
+        return message;
+    }
+}
