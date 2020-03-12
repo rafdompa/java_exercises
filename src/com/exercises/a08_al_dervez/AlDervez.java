779a25cf78a849b803379fab8a702445e2055cbc
Rafael Dominguez <rafa@macdaddy.fios-router.home>
2020-03-11 21:15:33 -0400

    added aldervez exercise

---
+++ src/com/exercises/a08_al_dervez/AlDervez.java
@@ -0,0 +1,28 @@
+package com.exercises.a08_al_dervez;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/*
+    INSTRUCTIONS:
+        Create a method that will reverse the phrase that is sent to it.
+    HINT:
+        * method name should be called 'reversePhrase'
+        * think about what type of argument is passed to it; what type of data
+            does it pass back to the caller
+        * the tests are currently commented out because they are configured to call the
+            reversePhrase method (which doesn't exist until you create it)
+            * go into AlDervezTest and uncomment the tests to validate your code; you
+            may want to uncomment only the first test and work through the problem
+            before uncommenting the rest of the tests
+    EXAMPLE:
+            |   INPUT             | OUTPUT                          |
+            |-------------------------------------------------------|
+            | Hello There         |  erehT olleH                    |
+            |-------------------------------------------------------|
+ */
+public class AlDervez {
+
+    //create method here
+}
---
+++ src/com/exercises/a08_al_dervez/AlDervezTest.java
@@ -0,0 +1,31 @@
+package com.exercises.a08_al_dervez;
+
+
+import static org.junit.Assert.*;
+
+public class AlDervezTest {
+
+//    @org.junit.Test
+//    public void testCase_01() {
+//        assertEquals("gfe dc ba",
+//                AlDervez.reversePhrase("ab cd efg"));
+//    }
+
+//    @org.junit.Test
+//    public void testCase_02() {
+//        assertEquals("!opih noc oreuqulep euq osorgilep sáM¡",
+//                AlDervez.reversePhrase("¡Más peligroso que peluquero con hipo!"));
+//    }
+
+//    @org.junit.Test
+//    public void testCase_03() {
+//        assertEquals("erbop ed aznarepse omoc ograL",
+//                AlDervez.reversePhrase("Largo como esperanza de pobre"));
+//    }
+
+//    @org.junit.Test
+//    public void testCase_04() {
+//        assertEquals("ohcnahc ed adatap omoc otitroC",
+//                AlDervez.reversePhrase("Cortito como patada de chancho"));
+//    }
+}
